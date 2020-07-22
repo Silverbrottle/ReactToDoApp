1a323d759b89cc0a54e919e2c6f2aafe2d5b2ff2
Silverbrottle <40502339+Silverbrottle@users.noreply.github.com>
2020-07-22 13:35:46 +0530

    Add files via upload

---
+++ build/precache-manifest.1eab0f624d485a4dcfd732c0f9f0e57b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddb62703a90db7e07d50293612d006f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a51f6a614231755478c",
+    "url": "/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "90be2eba491a3d96e106",
+    "url": "/static/js/2.96233b10.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/static/js/2.96233b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a51f6a614231755478c",
+    "url": "/static/js/main.05060274.chunk.js"
+  },
+  {
+    "revision": "26067943b6744300770a",
+    "url": "/static/js/runtime-main.8319d428.js"
+  }
+]);
